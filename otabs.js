daf42c9aafbee75a08d88f84feca156ce8ee71bf
Lorenzo Ganni <lorenzoganni@hotmail.it>
2016-03-24 00:17:05 +0100

    v1.2

--- otabs.js
+++ otabs.js
@@ -1,65 +1,92 @@
-/* oTabsJS - by Lorenzo Ganni - v1.1 [2015.3.12] */
+/* oTabsJS - by Lorenzo Ganni - v1.2 [2016.03.24] */
 (function () {
-  // initalizer
-  window.onload = function() {
-    // utils
-    function hasClass(element, cls) {
-      return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
-    }
-    function activateMenuOption(obj) {
-      obj.className += ' ' + sel_class;
-    }
-    function activateTabbedContent(obj,index) {
-      for (t in obj)
-        if (t != index)
-          obj[t].style.display = "none";
-        else
-          obj[t].style.display = "block";
-    }
-    function getMenuOptionIndex(elem){
-        var  i = 0;
-        while((elem = elem.previousElementSibling) != null) ++i;
-        return i;
-    }
-    // settings
-    var sel_class = "active";
-    // get objects
-    var $elems = document.querySelectorAll('ul.otabs');
+  // variables
+  var options, reversed, linked, n_items, id, $tabs;
+  var sel_class = "active";
 
-    for (i in $elems) {
-      // get info and objects
-      var $e = $elems[i];
-      var $menu = $e;
-      var $options = $e.getElementsByTagName('li');
-      var reversed = hasClass($menu,"reverse");
-      var n_items = $options.length - 1;
-      var id = $menu.getAttribute('rel');
-      var $tabs = (id == null) ?
-                    document.querySelectorAll(".otabs_content")[0].children
-                  :
-                    document.querySelectorAll(".otabs_content#"+id)[0].children;
-
-      // listener
-      for (o in $options) {
-        $options[o].onclick = function() {
-          // toogle 'active' class
-          for ($o in $options)
-            $options[$o].className = "";
-          activateMenuOption(this);
-          // get info and objects
-          var index = getMenuOptionIndex(this);
-          // display correct content
-          activateTabbedContent($tabs,(reversed) ? n_items - index : index);
+  // utils
+  function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+  }
+  function activateMenuOption(obj) {
+    obj.className += ' ' + sel_class;
+  }
+  function activateTabbedContent(obj,index) {
+    for (var i = 0; i < obj.length; i++)
+      if (i == index)
+        obj[i].style.display = "block";
+      else
+        obj[i].style.display = "none";
+  }
+  function getMenuOptionIndex(elem){
+      var  i = 0;
+      while((elem = elem.previousElementSibling) != null) ++i;
+      return i;
+  }
+  // get list item info
+  function getElementInfo(e) {
+    $options = e.children;
+    reversed = hasClass(e,"reverse");
+    linked = hasClass(e,"link");
+    n_items = $options.length - 1;
+    id = e.getAttribute('rel');
+    if (id != null) id = '#' + id;
+    $tabs = document.querySelectorAll(".otabs_content"+id)[0].children;
+  }
+  // change list and content selected item
+  function toggleSelection(index) {
+    // toogle 'active' class
+    for (var i = 0; i < $options.length; i++) {
+      $options[i].className = $options[i].className.replace(sel_class,"");
+    }
+    activateMenuOption($options[index]);
+    // choose linked tab if linked
+    if (linked) {
+      for (var i = 0; i <= n_items; i++) {
+        if ($tabs[i].getAttribute("id") == $options[index].getAttribute('rel')) {
+          tab_to_display = i;
         }
       }
+    }
+    else {
+      tab_to_display =  index;
+    }
+    // reverse selection if necessary
+    if (reversed) {
+      tab_to_display = n_items - tab_to_display;
+    }
+    activateTabbedContent($tabs, tab_to_display);
+  }
+
+  // initalizer
+  window.onload = function() {
+    // get all otabs menu in use
+    var $elems = document.querySelectorAll('ul.otabs');
+
+    for (e in $elems) {
+      // get info and objects
+      var $e = $elems[e];
+      getElementInfo($e);
+
       // menu element to be selected at the beginning
-      var init = $menu.dataset.select;
-      if (!/^[0-9\s\+]+$/.test(init)) init = 0;
-      if (init == "last" || init > n_items) init = n_items;
-      // init selection
+      var init = $e.dataset.select;
+      if (init == "last" || init > n_items)
+        init = n_items;
+      else if (!/^[0-9\s\+]+$/.test(init))
+        init = 0;
+
       activateMenuOption($options[init]);
-      // display correct content
-      activateTabbedContent($tabs,(reversed) ? n_items - init : init);
+      toggleSelection(init);
+
+      // onclick listener
+      for (o in $options) {
+        $options[o].onclick = function() {
+          // get info and objects
+          var index = getMenuOptionIndex(this);
+          getElementInfo(this.parentNode);
+          toggleSelection(index);
+        }
+      }
     }
   }
 })();
